64008d4b9bbcf27e8d49fbe88f1a6ca025dbd0eb
Ivan Timofeev <timofeevis89@gmail.com>
2024-05-31 12:52:50 +0300

    request sending changed

--- main.py
+++ main.py
@@ -21,27 +21,37 @@ async def get_lists(list_of_links, field_name, http_session):
     objects_array = [elem.get(f"{field_name}") for elem in result]
 
     return objects_array
+
+
 async def prepare_orm_obj(person, http_session):
     tasks = []
 
     films_task = None
     if person.get("films"):
-        films_task = asyncio.create_task(get_lists(person["films"], "title", http_session))
+        films_task = asyncio.create_task(
+            get_lists(person["films"], "title", http_session)
+        )
         tasks.append(films_task)
 
     species_task = None
     if person.get("species"):
-        species_task = asyncio.create_task(get_lists(person["species"], "name", http_session))
+        species_task = asyncio.create_task(
+            get_lists(person["species"], "name", http_session)
+        )
         tasks.append(species_task)
 
     starships_task = None
     if person.get("starships"):
-        starships_task = asyncio.create_task(get_lists(person["starships"], "name", http_session))
+        starships_task = asyncio.create_task(
+            get_lists(person["starships"], "name", http_session)
+        )
         tasks.append(starships_task)
 
     vehicles_task = None
     if person.get("vehicles"):
-        vehicles_task = asyncio.create_task(get_lists(person["vehicles"], "name", http_session))
+        vehicles_task = asyncio.create_task(
+            get_lists(person["vehicles"], "name", http_session)
+        )
         tasks.append(vehicles_task)
 
     await asyncio.gather(*tasks)
@@ -52,28 +62,32 @@ async def prepare_orm_obj(person, http_session):
     vehicles_list = vehicles_task.result() if vehicles_task else []
 
     orm_object = SwapiPeople(
-        birth_year=person.get('birth_year', 'n/a'),
-        eye_color=person.get('eye_color', 'n/a'),
+        birth_year=person.get("birth_year", "n/a"),
+        eye_color=person.get("eye_color", "n/a"),
         films=",".join(films_list),
-        gender=person.get('gender', 'n/a'),
-        hair_color=person.get('hair_color', 'n/a'),
-        height=person.get('height', 'n/a'),
-        homeworld=person.get('homeworld', 'n/a'),
-        mass=person.get('mass', 'n/a'),
-        name=person.get('name', 'n/a'),
-        skin_color=person.get('skin_color', 'n/a'),
+        gender=person.get("gender", "n/a"),
+        hair_color=person.get("hair_color", "n/a"),
+        height=person.get("height", "n/a"),
+        homeworld=person.get("homeworld", "n/a"),
+        mass=person.get("mass", "n/a"),
+        name=person.get("name", "n/a"),
+        skin_color=person.get("skin_color", "n/a"),
         species=",".join(species_list),
         starships=",".join(starships_list),
         vehicles=",".join(vehicles_list),
     )
     return orm_object
 
+
 async def insert_to_db(list_of_people, http_session):
     async with Session() as session:
-        person_coros = [prepare_orm_obj(person, http_session) for person in list_of_people]
+        person_coros = [
+            prepare_orm_obj(person, http_session) for person in list_of_people
+        ]
         orm_obj_list = await asyncio.gather(*person_coros)
         session.add_all(orm_obj_list)
         await session.commit()
+
 
 async def main(base_path):
     await init_db()
